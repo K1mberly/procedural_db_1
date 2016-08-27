8937a7f803a4cd5713584483ca88134548ac4285
K1mberly <kim.ruiz.g@gmail.com>
2016-08-26 22:11:41 -0500

    option button

--- database1/crud/option_button_example.php
+++ database1/crud/option_button_example.php
@@ -5,11 +5,14 @@
     <title></title>
   </head>
   <body>
-
+    <form>
+      <input type="radio" name="ejemplo1" value="option1">Option 1<br>
+      <input type="radio" name="ejemplo1" value="option2" checked>Option 2<br>
+    </form>
     <script type="text/javascript" src="">
       $(document).ready(function() {
-        //alert("Esto es una alerta");
-        console.log("documento cargado");
+        alert("Esto es una alerta");
+        //console.log("documento cargado");
 
       });
 
